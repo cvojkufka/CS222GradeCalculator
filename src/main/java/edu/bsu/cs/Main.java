2fa2660370ad562a9b6818b92f014fddb71e20e3
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2025-03-17 10:37:12 -0400

    created menu

---
+++ src/main/java/edu/bsu/cs/Main.java
@@ -0,0 +1,36 @@
+package edu.bsu.cs;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            printMenu();
+            String choice = scanner.nextLine();
+
+            if(choice.equals("1")) {
+                calculateCourseGrade();
+            } else if(choice.equals("2")) {
+                break;
+            } else {
+                System.out.println("Invalid choice. Please enter a 1 or 2");
+            }
+        }
+
+    }
+
+    public static void printMenu() {
+        System.out.println("===== CS 222 Course Grade Calculator =====");
+        System.out.println("1) Start Calculating Course Grade");
+        System.out.println("2) Exit the program");
+        System.out.print(">>");
+    }
+
+    public static void calculateCourseGrade() {
+        CourseCalculator gradeCalculator = new CourseCalculator();
+        gradeCalculator.calculateGrade('A');
+    }
+}
